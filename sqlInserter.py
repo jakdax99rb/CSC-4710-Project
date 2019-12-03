6c2408934a3902128eab873a289df81659e8cf28
jakdax99rb <jakdax99rb@gmail.com>
2019-12-03 15:32:18 -0500

    Just moved things around and added notes to the sqlInserter

--- sqlInserter.py
+++ sqlInserter.py
@@ -18,3 +18,12 @@ cnx.commit()
 
 cursor.close()
 cnx.close()
+
+
+# Insert everything as an item first so I can do foreign key stuff easily.
+# If a dictionary contains either vertical or horizontal recoil then its a weapon,
+# if a dictionary contains a recoil modifier or a ergo modifier it is an attachment
+# If a dictionary has suppressor as its type then it goes in the supressor entity
+# If a dictionary contains class then it will go into armor.
+# For penetration I will need to iterate through all armors for every caliber to determine penetration
+# to determine penetration I will just check to see if the penetration value of the round divided by 10 is greater than the class of the armor its against.
